a5dde770e2d3d9801f99a7766f343e8a8ec2119c
mankondavivek <mvivek.1694@gmail.com>
2018-01-19 12:12:36 +0530

    add few more files

---
+++ src/main/java/com/project/quiz/app/controller/WelcomeController.java
@@ -0,0 +1,24 @@
+package com.project.quiz.app.controller;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+	
+	
+	/**
+	 * The below method redirects control to the "begintest.html" page, thereby
+	 *	preventing "begintest.html" from being specified explicitly in the URL
+	 * @param response
+	 * @throws IOException
+	 */
+	@RequestMapping(value = "/")
+	public void redirectToLogin(HttpServletResponse response) throws IOException {
+		response.sendRedirect("/begintest.html");
+	}
+}
