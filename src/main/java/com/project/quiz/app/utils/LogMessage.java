f1f6ed84b21820333d1c3b770c7473e2a56ec18f
mankondavivek <mvivek.1694@gmail.com>
2018-02-16 16:29:42 +0530

    Added LogMessage Util

--- src/main/java/com/project/quiz/app/utils/LogMessage.java
+++ src/main/java/com/project/quiz/app/utils/LogMessage.java
@@ -6,16 +6,12 @@ public class LogMessage {
 	public static final String hitController = "Hit Controller Endpoint";
 	public static final String hitFacade = "Hit Facade Endpoint";
 	public static final String hitService = "Hit Service Endpoint";
-	public static final String hitFacadeImpl = "Hit FacadeImpl Endpoint";
-	public static final String hitServiceImpl = "Hit ServiceImpl Endpoint";	
 	public static final String hitException = "Hit Exception Endpoint";
 	public static final String hit = "Hit  Endpoint";
 	
 	public static final String exitController = "Exit Controller Endpoint";
 	public static final String exitFacade = "Exit Facade Endpoint";
 	public static final String exitService = "Exit Service Endpoint";
-	public static final String exitFacadeImpl = "Exit FacadeImpl Endpoint";
-	public static final String exitServiceImpl = "Exit ServiceImpl Endpoint";	
 	public static final String exitException = "Exit Exception Endpoint";
 	public static final String exit = "Exit  Endpoint";
 	
